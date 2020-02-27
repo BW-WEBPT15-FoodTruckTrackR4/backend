5a227de1654a2e845745d7f55a741071ec4e1055
Mary <mary.clayton1@hotmail.com>
2020-02-27 16:13:06 -0700

    fixed operator login

--- auth/operator-router.js
+++ auth/operator-router.js
@@ -24,9 +24,10 @@ router.post('/login', (req, res) => {
     .first()
     .then(user => {
         if (user && bcrypt.compareSync(password, user.password)) {
-            req.session.user = user;
+            const token = genToken(user)
             res.status(200).json({
-                message: `Welcome ${user.username}!`
+                message: `Welcome ${user.username}!`,
+                token: token
             })
         } else {
             res.status(401).json({
@@ -39,5 +40,16 @@ router.post('/login', (req, res) => {
     })
 })
 
+function genToken(user) {
+    const payload = {
+        userId: user.id,
+        username: user.username
+    }
+    const options = {
+        expiresIn: '1h'
+    }
+    const token = jwt.sign(payload, secrets.jwtSecret, options)
+    return token;
+}
 
 module.exports = router;

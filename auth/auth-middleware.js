75b17fbda6c9157983ee6535353d09108860fc54
Mary <mary.clayton1@hotmail.com>
2020-02-27 15:32:31 -0700

    json web tokens

--- auth/auth-middleware.js
+++ auth/auth-middleware.js
@@ -1,8 +1,24 @@
 module.exports = (req, res, next) => {
-    if(req.session && req.session.user){
-      next();
-    } else {
-    res.status(401).json({ you: 'shall not pass!' });
-    }
-  };
+    // if(req.session && req.session.user){
+    //   next();
+    // } else {
+    // res.status(401).json({ you: 'shall not pass!' });
+    // }
+   
+      const token = req.headers.authorization
+  
+      if(token) {
+          jwt.verify(token, secrets.jwtSecret, (err, decodedToken) => {
+              if (err) {
+                  res.status(401).json({ message: 'you shall not pass!'})
+              } else {
+                  req.decodedJwt = decodedToken
+                  next();
+              } 
+          })
+      } else {
+          res.status(401).json({ message: " you cant touch that"})
+      }
+  }
+
   

2d384f049a7f3802ddab533bbb8bcb76c9564ff2
Mary <mary.clayton1@hotmail.com>
2020-03-05 15:41:54 -0700

    fixed seeds

--- data/seeds/04-auth.js
+++ data/seeds/04-auth.js
@@ -6,9 +6,9 @@ exports.seed =
   //   .then(function () {
       // Inserts seed entries
       return knex('diners').insert([
-        {username: 'foodiefan1', password: '$2a$09$fMUtRzYrLdRrDFHrRq7Ix.UkOcFxlkCENC0vprxMdYNOvp4jILiTy', currentLocation: 'Phoenix, AZ', favoriteTrucks: ["Joe's Tacos", "The Gorilla Cheese Truck", "Taco Toppers"]}, // ilovefood
-        {username: 'foodskills4', password: '$2a$09$eBqhxncQhLtxd0Mx7TUAUuN1rBZA3LFMrs2KKDGqbLVe5RDjEc7j6', currentLocation: 'Seattle, WA', favoriteTrucks: ["Yumbit", "Samurai Noodle Food Truck", "Grumpys Food Truck"]},
-        {username: 'ilovefood36', password: '$2a$09$tCJK6noDBxbZN1aLD3iYqOhxV9nL3Dp3pe2XxQcHGAKX2ww9ZdFvy', currentLocation: 'Chicago, IL', favoriteTrucks: ["Chicago Lunchbox", "Flash Taco Food Truck"]}
+        {username: 'foodiefan1', password: '$2a$09$fMUtRzYrLdRrDFHrRq7Ix.UkOcFxlkCENC0vprxMdYNOvp4jILiTy', currentLocation: 'Phoenix, AZ', favoriteTrucks: "Joe's Tacos, The Gorilla Cheese Truck, Taco Toppers"}, // ilovefood
+        {username: 'foodskills4', password: '$2a$09$eBqhxncQhLtxd0Mx7TUAUuN1rBZA3LFMrs2KKDGqbLVe5RDjEc7j6', currentLocation: 'Seattle, WA', favoriteTrucks: "Yumbit, Samurai Noodle Food Truck, Grumpys Food Truck"},
+        {username: 'ilovefood36', password: '$2a$09$tCJK6noDBxbZN1aLD3iYqOhxV9nL3Dp3pe2XxQcHGAKX2ww9ZdFvy', currentLocation: 'Chicago, IL', favoriteTrucks: "Chicago Lunchbox, Flash Taco Food Truck"}
       ]);
     // })
   }

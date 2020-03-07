9f62bdf5630c8cacebd7d047ec224688b674b281
Mary <mary.clayton1@hotmail.com>
2020-03-06 21:33:23 -0700

    test

--- knexfile.js
+++ knexfile.js
@@ -3,7 +3,7 @@
 module.exports = {
   development: {
     client: 'pg',
-    connection:'postgres://yvwlnpvrhdgipx:848cbce9bcef702f9e6249ec1ae63030f29affd1d6efd3c5b7d91e7877ead0bd@ec2-35-168-54-239.compute-1.amazonaws.com:5432/dducu3uujr8iko',
+    connection:process.env.DATABASE_URL,
     migrations: {
       directory: './data/migrations'
     },
